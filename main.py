c29f51d47e4eb2669bea80bd5988ef1334dc2daf
Nikint86 <lobackiy_2007@mail.ru>
2024-11-08 23:40:39 +0300

    код

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def weather(city):
+    payload = {"n": "",
+           "T": "",
+           "q": "",
+           "m": "",
+           "M": "",
+           "lang": "ru"}
+    adress_global = 'https://wttr.in/{}'
+    url = adress_global.format(city)
+    response = requests.get(url, params=payload)
+    return response.text
+if __name__ == '__main__':
+    adress_local = ['svo','Париж','Череповец']
+
+for city in adress_local:
+    print(weather(city))
